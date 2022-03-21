280c69df031bae783f1f295777ba5fd5db8fd116
Lzp-Code <lu-zhipeng@foxmail.com>
2022-03-21 16:38:45 +0800

    ....

--- application/index/command/LzpTest.php
+++ application/index/command/LzpTest.php
@@ -23,6 +23,10 @@ class LzpTest extends Command
      *
      * 在服务器写入定时的时间，并指向该命令
      * 比如示例——每小时的第15分钟执行一次：15 * * * * php /data/www/hebei/think LzpTest（按照执行命令行的模式：php think LzpTest，只是这里要指向任务所在的文件夹，所以写成了php /data/www/hebei/think LzpTest）
+     * 然后重启：/bin/systemctl restart crond.service
+     * 如果执行过程中要停止，需要杀进程：
+     * ps -ef | grep LzpTest    然后  kill 进程号
+     *
      *
      * 制定一个自己的定时任务：
      *https://blog.csdn.net/asasasasaq/article/details/90693570
