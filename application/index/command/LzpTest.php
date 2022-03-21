5cbf89fb6b29adf8ec1601bd2c5a638ec231c608
Lzp-Code <lu-zhipeng@foxmail.com>
2022-03-21 15:48:44 +0800

    ....

--- application/index/command/LzpTest.php
+++ application/index/command/LzpTest.php
@@ -14,6 +14,24 @@ use think\Db;
 
 class LzpTest extends Command
 {
+
+
+
+    /**
+     * 在command目录写入要执行的逻辑
+     * 然后在command.php写好命令行
+     *
+     * 在服务器写入定时的时间，并指向该命令
+     * 比如示例——每小时的第15分钟执行一次：15 * * * * php /data/www/hebei/think LzpTest（按照执行命令行的模式：php think LzpTest，只是这里要指向任务所在的文件夹，所以写成了php /data/www/hebei/think LzpTest）
+     *
+     * 制定一个自己的定时任务：
+     *https://blog.csdn.net/asasasasaq/article/details/90693570
+     * Crontab命令参考：
+     * https://cloud.tencent.com/developer/article/1666275
+     *
+     */
+
+
     protected function configure()
     {
         $this->setName('LzpTest')->setDescription('测试测试测试');
@@ -21,15 +39,10 @@ class LzpTest extends Command
 
     protected function execute(Input $input, Output $output)
     {
-
-
             $data = array();
             $data['question'] = time();
             $data['update_time'] = date('Y-m-d H:i:s', time());
             $data['create_time'] = date('Y-m-d H:i:s', time());
             Db::name('appeal_safety_question')->data($data)->insert();
-
-
-
     }
 }
