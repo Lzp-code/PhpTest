6e2b48665d8d418b5ae4dccd3bbf4ea6870e8bb9
Lzp-Code <lu-zhipeng@foxmail.com>
2022-03-22 16:01:42 +0800

    ....

--- application/index/command/LzpTest.php
+++ application/index/command/LzpTest.php
@@ -23,15 +23,16 @@ class LzpTest extends Command
      *
      * 安装crontab
      * yum install crontabs
-     * crontab -l
-     * crontab -e
-     * crontab -r
-     * crontab -i
+     * crontab -l   查看当前用户定时任务列表
+     * crontab -e   编辑当前用户定时任务列表
+     * crontab -r   删除用户定时任务文件
+     * crontab -i   删除用户定时任务文件时给出确认
      *
      *
      *
      * 在服务器写入定时的时间，并指向该命令
      * 比如示例——每小时的第15分钟执行一次：15 * * * * php /data/www/hebei/think LzpTest（按照执行命令行的模式：php think LzpTest，只是这里要指向任务所在的文件夹，所以写成了php /data/www/hebei/think LzpTest）
+     * crontab 中的 command 尽量使用绝对路径，否则会经常因为路径错误导致任务无法执行。
      * 然后重启：/bin/systemctl restart crond.service
      * 如果执行过程中要停止，需要杀进程：
      * ps -ef | grep LzpTest    然后  kill 进程号
@@ -41,7 +42,8 @@ class LzpTest extends Command
      *https://blog.csdn.net/asasasasaq/article/details/90693570
      * Crontab命令参考：
      * https://cloud.tencent.com/developer/article/1666275
-     *
+     *  https://www.cnblogs.com/ftl1012/p/crontab.html
+     * https://zhuanlan.zhihu.com/p/413666659
      */
 
 
